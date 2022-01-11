12ca609dd6a9aed7e681975477936eb7db209540
Santiago Bernal G <santiagobernal3@gmail.com>
2022-01-11 18:06:00 -0500

    m3-t2

---
+++ js/lang.es.js
@@ -0,0 +1,107 @@
+// This file is part of CDI Tool
+//
+// CDI is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// CDI is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with CDI.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings, language 'es'
+ *
+ * Use with dhbgApp.s('key', [var | object]).
+ *
+ */
+dhbgApp.strings = {
+    'accept': 'Aceptar',
+
+    'activities_attempts': 'no tienes intentos',
+
+    'after': 'Después',
+
+    'all_correct': 'Felicitaciones, has completado la actividad de manera satisfactoria',
+
+    'all_correct_percent': '¡Muy bien! El 100% de tus respuestas son correctas.',
+
+    'all_wrong': 'El {0}% de tus respuestas son incorrectas. Repasa el contenido e intenta realizar de nuevo esta actividad.',
+
+    'answer_corrent': 'La respuesta es correcta',
+
+    'answer_required': 'Por favor, asigna una respuesta a cada ítem.',
+
+    'answer_wrong': 'La respuesta es incorrecta',
+
+    'before': 'Antes',
+
+    'click_to_open': 'Haz clic acá para abrir el documento de estudio',
+
+    'cloze_required': 'Por favor, llena todos los espacios en blanco para continuar.',
+
+    'continue_activity': 'Continúa con la actividad',
+
+    'drop_required': 'Por favor, arrastra todos los elementos a alguno de los destinos válidos',
+
+    'ended_not_close': 'Has cerrado la ventana del documento, si deseas volverlo a abrir, utiliza el siguiente botón.',
+
+    'form_full': 'Todos los campos han sido diligenciados.',
+
+    'form_required': 'Debes diligenciar todos los campos.',
+
+    'horizontal': 'Horizontal',
+
+    'loading': 'Cargando...',
+
+    'mark_required': 'Necesitas marcar por lo menos una zona para continuar.',
+
+    'none': 'Ninguno',
+
+    'opened': 'Este documento de estudio ha sido abierto en una ventana emergente.',
+
+    'opened_return': 'Este documento de estudio ha sido abierto en una ventana emergente. Si ya has finalizado de interactuar con el recurso, da clic acá para regresar a la pagina principal del curso.',
+
+    'pagination_label': '{a} de {b}',
+
+    'play_animation': 'Clic para reproducir la animación',
+
+    'popups': 'Parece que las ventanas emergentes están bloqueadas, deteniendo la ejecución de este documento de estudio.',
+
+    'print_version': 'Vista de impresión',
+
+    'progress': 'Avance',
+
+    'repeat_animation': 'Clic para repetir la animación',
+
+    'restart_activity': 'Reiniciar la actividad',
+    'end_activity': 'Terminar',
+
+    'result_to': '{a} de {b}',
+
+    'save': 'Guardar',
+
+    'save_correct': 'Se ha guardado la información correctamente',
+
+    'save_wrong': 'No se ha podido guardar la información, por favor verifica tu conexión a Internet e intenta de nuevo más tarde.',
+
+    'score': 'Encontradas:',
+
+    'scorm_not': 'Este documento se encuentra en modo de solo lectura por lo que la información de lo que realices no se guardará. Si precisas almacenar el seguimiento, por favor cierra esta página e intenta más tarde.',
+
+    'selected_required': 'Necesitas seleccionar una palabra de la lista para continuar',
+
+    'start_activity': 'Iniciar la actividad',
+
+    'verify': 'Verificar',
+
+    'vertical': 'Vertical',
+
+    'wrong_continue': 'El {0}% de tus respuestas son incorrectas. Repasa el contenido e intenta realizar de nuevo esta actividad.',
+
+    'wrong_percent': 'El {0}% de tus respuestas son incorrectas. Repasa el contenido e intenta realizar de nuevo esta actividad.'
+};
